07ccfa87c588208b37d4ebcbe7e34a20f4223bc5
Irina-Alieva <irina-aliyeva@yandex.ru>
2019-09-19 18:11:55 +0300

    release v.1.1

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@
 	'use strict'
 
 	let plateNumber = 1 //  текущая карточка
-
-let progressBar =  25 * (plateNumber-1) + '%'
-let progressNumber = 25 * (plateNumber-1)
-
+ 	let progressBar =  0 + '%'
+	let progressNumber = 0
+	let progressBarElem = document.querySelectorAll('.progress__line-bar')
+	let progressElem = document.querySelectorAll('.progressNumber')
 	const answers = [null, null, null, null] // массив ответов
 	//Инициализация стартовой карточки
 	switchPlate (plateNumber)
@@ -18,7 +18,6 @@ let progressNumber = 25 * (plateNumber-1)
 	}
 
 	// Обработка радио-кнопок 2-ой карточки
-
 	const labelElements_2 = document.querySelectorAll('#plate2 label')
 	for (const labelElement of labelElements_2) {
 		labelElement.addEventListener('click', labelElementClickEventHandler_2)
@@ -27,23 +26,23 @@ let progressNumber = 25 * (plateNumber-1)
 
 	function labelElementClickEventHandler_2 (event){
 		event.stopPropagation()
-
 		for (const labelElement of labelElements_2){
-		labelElement.classList.remove('radio-block--active')	
+			labelElement.classList.remove('radio-block--active')	
 		}
-
 		this.classList.add('radio-block--active')
 		answers[0] = this.querySelector('.radio-block__text').textContent
+		const buttonNext = document.querySelectorAll('.button--next')	
+		buttonNext[0].style.backgroundColor ='#09ac0c'	
+		progressNumber = 25		
+		progressBarElem[0].style.width = progressNumber + '%'
+		progressElem[0].textContent = progressNumber + '%'
 	}
-
-
 
 	// Обработка чек-боксов 3-ой карточки
 	const labelElements_3 = document.querySelectorAll('#plate3 label')
 	for (const labelElement of labelElements_3) {
 		labelElement.addEventListener('click', labelElementClickEventHandler_3)
 		labelElement.classList.remove('checkbox-block--active')
-
 	}
 	let flag = false
 	function labelElementClickEventHandler_3 (event){
@@ -59,7 +58,11 @@ let progressNumber = 25 * (plateNumber-1)
 		}
 
 		answers[1] = checked.length === 0 ? null : checked
-		
+		const buttonNext = document.querySelectorAll('.button--next')
+		buttonNext[1].style.backgroundColor ='#09ac0c'
+		progressNumber = 50
+		progressBarElem[1].style.width = progressNumber + '%'
+		progressElem[1].textContent = progressNumber + '%'
 	}
 
 	// Обработка радио-кнопок 4-ой карточки
@@ -76,17 +79,33 @@ let progressNumber = 25 * (plateNumber-1)
 
 		this.classList.add('card-block--active')
 		answers[2] = this.querySelector('.card-block__text').textContent
+		const buttonNext = document.querySelectorAll('.button--next')
+		buttonNext[2].style.backgroundColor ='#09ac0c'
+		progressNumber = 75		
+		progressBarElem[2].style.width = progressNumber + '%'
+		progressElem[2].textContent = progressNumber + '%'	
 	}
 
 	// Обработка 5-ой карточки
-	const inputElement = document.querySelector('#email')
+	const inputEmail = document.querySelector('#email')
 	const resultElement = document.querySelector('#result')
-	const checkboxElement  = document.querySelector('#politics') 
+	const checkRules  = document.querySelector('#checkRules')
+	checkRules.addEventListener('click', function(event){
+		if (inputEmail.value !== ''){
+			resultElement.style.backgroundColor = '#09ac0c'
+			}
+	})
+
+	inputEmail.addEventListener('change', function(event){
+		if (checkRules.checked){
+				resultElement.style.backgroundColor = '#09ac0c'
+		}
+	})
+
 	resultElement.onclick = function quizResult(event){
 		event.preventDefault()
 		event.stopPropagation()
-		
-		if ( inputElement.value !== '' && checkboxElement.checked){
+		if ( inputEmail.value !== '' && checkRules.checked){
 			switchPlate(6)	
 		}
 	}
@@ -111,7 +130,6 @@ let progressNumber = 25 * (plateNumber-1)
 		showPlate (number)
 	}
 
-
 	function hiddenAllPlates (){
 
 		const plateElements = document.querySelectorAll('.plate')
@@ -125,7 +143,5 @@ let progressNumber = 25 * (plateNumber-1)
 		const plateElement = document.querySelector('#plate' + number)
 		plateElement.style.display = ''
 	}
-
-	
 
 }) ();
